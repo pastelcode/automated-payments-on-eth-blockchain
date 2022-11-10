a714c54bb728882e78a341450f189642bf20660b
Pastel <pastel.dev.gt@gmail.com>
2022-11-10 12:30:07 -0600

    Create the `Stage` smart contract

--- contracts/states/Stages.sol
+++ contracts/states/Stages.sol
@@ -1,4 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract Stages {}
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Stage is Ownable {
+    enum Stages {
+        CREATION,
+        SIGN,
+        RUNNING,
+        FINISH
+    }
+
+    Stages public stage = Stages.CREATION;
+
+    modifier atStage(Stages _stage) {
+        require(stage == _stage, "Function cannot be called at this time.");
+        _;
+    }
+
+    function setStage(Stages _stage) internal {
+        stage = _stage;
+    }
+
+    function destruct() internal {
+        selfdestruct(payable(owner()));
+    }
+}
