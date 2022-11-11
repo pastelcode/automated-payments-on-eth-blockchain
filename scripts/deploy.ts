456b2aa1470aa36ed5cd0b8cd23d2ee4d856a270
Pastel <pastel.dev.gt@gmail.com>
2022-11-11 14:46:37 -0600

    Add logs for deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,32 +1,64 @@
 import { ethers } from 'hardhat'
 
 async function main() {
+  process.stdout.write('Getting accounts... ')
   const [deployer, firstMember, secondMember, thirdMember, fourthMember] =
     await ethers.getSigners()
-  console.log(`Deploying with account: ${deployer.address}`)
+  console.log('[DONE]')
+  console.log(`Deploying with account: ${deployer.address}\n`)
 
+  process.stdout.write('Deploying main contract... ')
   const Deal = await ethers.getContractFactory('TimeDispersions')
   const deal = await Deal.deploy()
+  console.log('[DONE]')
+  console.log(`Contract address: ${deal.address}\n`)
 
+  const ethQuatityForContract = 5
+  process.stdout.write(`Sending ${ethQuatityForContract} ETH to contract... `)
   await deployer.sendTransaction({
     to: deal.address,
-    value: ethers.utils.parseEther('5'),
+    value: ethers.utils.parseEther(`${ethQuatityForContract}`),
   })
+  console.log('[DONE]\n')
 
   // Register members to contract (address, percent), if percent is
   // equal to 0, the percentage will be calculated automatically based
   // on the existing ones. Percent sum must always be equal to 100.
-  await deal.registerMember('0x70997970C51812dc3A010C7d01b50e0d17dc79C8', 10)
-  await deal.registerMember('0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC', 10)
-  await deal.registerMember('0x90F79bf6EB2c4f870365E785982E1f101E93b906', 10)
-  await deal.registerMember('0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65', 0) // Automatically set to 70
+  console.log('Registering members... ')
+  const members = [
+    {
+      address: firstMember.address,
+      percent: 10,
+    },
+    {
+      address: secondMember.address,
+      percent: 10,
+    },
+    {
+      address: thirdMember.address,
+      percent: 10,
+    },
+    {
+      address: fourthMember.address,
+      percent: 0, // Automatically set to 70
+    },
+  ]
+  for (const member of members) {
+    const { address, percent } = member
+    process.stdout.write(`${address} (${percent}%)... `)
+    await deal.registerMember(address, percent)
+    console.log('[DONE]')
+  }
+  console.log()
 
   // Set duration of contract
+  process.stdout.write('Setting duration for contract (4 months)... ')
   // await deal.setEnd(4, web3.utils.fromUtf8('month'))
   await deal.setEnd(
     4,
     '0x6d6f6e7468000000000000000000000000000000000000000000000000000000'
   )
+  console.log('[DONE]\n')
 
   // Set time lapseds (percents, value, unit)
   //
@@ -34,19 +66,27 @@ async function main() {
   // Second month to 10% of total of ethers
   // Third month to 10% of total of ethers
   // Fourth month to 70% of total of ethers
+  process.stdout.write('Setting lapseds... ')
   await deal.setLapseds(
     [10, 10, 10, 70],
     1,
     /*web3.utils.fromUtf8('month')*/ '0x6d6f6e7468000000000000000000000000000000000000000000000000000000'
   )
+  console.log('[DONE]\n')
 
   // Sign all members
   // After all members are signed, the contract automatically
   // executes `exec`.
-  await deal.sign(firstMember.address)
-  await deal.sign(secondMember.address)
-  await deal.sign(thirdMember.address)
-  await deal.sign(fourthMember.address)
+  console.log('Signing members...')
+  for (const member of members) {
+    const { address } = member
+    process.stdout.write(`${address}... `)
+    await deal.sign(address)
+    console.log('[DONE]')
+  }
+  console.log()
+
+  console.log('Contract running!')
 }
 
 ;(() => {
